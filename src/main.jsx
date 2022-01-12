a6e642fc573101c1fd4248a59afd0d354aa8b432
deankwan@163.com <deankwan@163.com>
2022-01-12 23:21:45 +0800

    feat: project initial

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./page/home/App";
+
+import "normalize.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/page/home/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>aaaa</div>;
+};
+
+export default App;
